43134921df7ff9608c7def08d73c9b2fa8bebec6
zubowaantonina <zubowaantonina@yandex.ru>
2025-10-22 22:18:07 +0300

    React Router (новый синтаксис)

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom"
 import { ToDo } from "../../models/todo-item"
 import classes from './ListItem.module.scss'
 
-export const ListItem=({todo}:{todo:ToDo})=>{
-    return(
-      
-        <a 
-        className={`${classes.link} ${todo.isDone?classes.done:classes.notDone}`} 
-        href={`/list/${todo.id}`}
-        >{todo.text}</a>
-        
+export const ListItem = ({ todo }: { todo: ToDo }) => {
+    return (
+
+        <Link
+            className={`${classes.link} ${todo.isDone ? classes.done : classes.notDone}`}
+            to={`/list/${todo.id}`}
+        >{todo.text}
+        </Link>
+
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './assets/scss/normalize.scss';
 import './assets/scss/style.scss';
 import { ToDoListPage } from './pages/ToDoListPage'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import {  createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { HomePage } from './pages/HomePage';
-import { Header } from './components/Header/Header';
 import { ToDo } from './models/todo-item';
 import { NotFound } from './pages/404';
 import { ItemDescription } from './pages/ItemDescription';
+import { Layout } from './layouts/Layout';
+
+
 
 const todos: ToDo[] = [
   {
@@ -32,21 +34,40 @@ const todos: ToDo[] = [
     isDone: true
   }
 ]
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout/>,
+    errorElement:<NotFound />,
+    children:[
+      {
+        path:'/',
+        element:<HomePage todos={todos} />
+      },
+       {
+        path:'/todo',
+        element:<ToDoListPage />
+      },
+      {
+        path:'/list/:id',
+        element:<ItemDescription todos={todos} />
+      }
+    ]
+  },
+  {
+    path:'*',
+    element:<NotFound />
+  }
+// ], {basename: process.env.NODE_ENV === 'development' ? '/' : '/todo-rdx/'}
+],{basename: '/app/'}
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<HomePage todos={todos} />}></Route>
-        <Route path='/list/:id' element={<ItemDescription todos={todos} />}></Route>
-        <Route path='/todo' element={<ToDoListPage />}></Route>
-        <Route path='*' element={<NotFound />}></Route>
-      </Routes>
-    </BrowserRouter>
-
+    <RouterProvider router={router}/>
+    
   </React.StrictMode>
 )
 
