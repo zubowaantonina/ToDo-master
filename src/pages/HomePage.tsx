d54f661a365d182eff6a2e348bcaa6f1809bf5a2
zubowaantonina <zubowaantonina@yandex.ru>
2025-10-22 22:19:47 +0300

    React Router (новый синтаксис)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,14 @@
 import { ListItem } from "../components/ListItem/ListItem"
 import { ToDo } from "../models/todo-item"
-interface ComponentProps{
-    todos:ToDo[]
+interface ComponentProps {
+    todos: ToDo[]
 }
-export const HomePage=({todos}:ComponentProps)=>{
-    return(
-       <div className="container">
-       {todos.map((todo:ToDo, idx:number)=>{
-        return(<ListItem todo={todo} key={idx}/>)
-       })}
-       </div>
+export const HomePage = ({ todos }: ComponentProps) => {
+    return (
+        <div className="container">
+            {todos.map((todo: ToDo, idx: number) => {
+                return (<ListItem todo={todo} key={idx} />)
+            })}
+        </div>
     )
 }
